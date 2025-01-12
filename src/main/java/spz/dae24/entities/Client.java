b4ae75595a13c3d7abc4e35b4dd7b7b3bd18afaa
SimaoAntunes <simao.martins.antunes@hotmail.com>
2025-01-12 13:13:27 +0000

    I think clients is done, needs testing

--- src/main/java/spz/dae24/entities/Client.java
+++ src/main/java/spz/dae24/entities/Client.java
@@ -1,7 +1,35 @@
 package spz.dae24.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.OneToMany;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(
+                name = "getAllClients",
+                query = "SELECT new Client(c.id,c.username,c.name,c.email) FROM Client c"
+        )
+})
 public class Client extends User {
+    @OneToMany(mappedBy = "client")
+    private final List<Package> packages = new ArrayList<>();
+
+    public Client() {}
+
+    public Client(int id, String username, String name, String email) {
+        super(id, username, name, email);
+    }
+
+    public List<Package> getPackages() {
+        return packages;
+    }
+
+    public boolean addPackage(Package pack) {
+        return packages.add(pack);
+    }
 }
---
+++ src/main/java/spz/dae24/ws/ClientService.java
@@ -0,0 +1,35 @@
+package spz.dae24.ws;
+
+import jakarta.ejb.EJB;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import spz.dae24.dtos.ClientDTO;
+import spz.dae24.ejbs.ClientBean;
+
+import java.util.List;
+
+
+@Path("clients")
+@Produces({MediaType.APPLICATION_JSON})
+@Consumes({MediaType.APPLICATION_JSON})
+public class ClientService {
+    @EJB
+    private ClientBean clientBean;
+
+    @GET
+    @Path("")
+    public List<ClientDTO> getAllClients() {
+        return ClientDTO.from(clientBean.findAll());
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getClient(@PathParam("id") long id) {
+        var client = clientBean.find(id);
+        var clientDTO = ClientDTO.from(client);
+
+        return Response.ok(clientDTO).build();
+    }
+
+}
