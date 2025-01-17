0197a98960846c60a509e6f0b1130d1f9e6fccd7
Gameiro <alexandrefgame@gmail.com>
2025-01-17 14:16:14 +0000

    Added toast to auth login

--- src/main/java/spz/dae24/ws/ClientService.java
+++ src/main/java/spz/dae24/ws/ClientService.java
@@ -3,10 +3,8 @@ package spz.dae24.ws;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.EJB;
 import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.SecurityContext;
 import spz.dae24.dtos.ClientDTO;
 import spz.dae24.ejbs.ClientBean;
 import spz.dae24.security.Authenticated;
@@ -21,9 +19,6 @@ import java.util.List;
 public class ClientService {
     @EJB
     private ClientBean clientBean;
-
-    @Context
-    private SecurityContext securityContext;
 
     @GET
     @Path("")
@@ -40,18 +35,5 @@ public class ClientService {
         var clientDTO = ClientDTO.from(client);
 
         return Response.ok(clientDTO).build();
-    }
-
-    @GET
-    @Path("{username}")
-    public Response getMyClient(@PathParam("username") String username) {
-        var principal = securityContext.getUserPrincipal();
-
-        if(!principal.getName().equals(username)) {
-            return Response.status(Response.Status.FORBIDDEN).build();
-        }
-
-        var client = clientBean.find(username);
-        return Response.ok(ClientDTO.from(client)).build();
     }
 }
--- vue/src/stores/auth.js
+++ vue/src/stores/auth.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 import { defineStore } from "pinia";
 import { ref, computed } from "vue";
+import { useErrorStore } from "./error";
 
 
 export const useAuthStore = defineStore('auth', () => {
+  const errorStore = useErrorStore()
+
   const token = ref('')
   const username = ref('')
 
   const login = async (credentials) => {
     try {
+      errorStore.resetErrorMessage()
       const tokenResponse = await axios.post('auth/login', {
         username: credentials.username,
         password: credentials.password
@@ -16,9 +20,14 @@ export const useAuthStore = defineStore('auth', () => {
       token.value = tokenResponse.data
       axios.defaults.headers.common.Authorization = 'Bearer ' + token.value
       username.value = credentials.username
+      
       return true
-
     } catch (e) {
+      errorStore.setErrorMessage(
+        e.response.status,
+        e.response.statusText,
+        " Could not login with credentials"
+      )      
       return false
     }
   }
